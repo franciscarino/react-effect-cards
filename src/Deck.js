4732b8dad1fbbd50eab018db7a02d0e0e07ba376
Francis Carino <fgcarino@gmail.com>
2022-09-06 11:47:52 -0700

    add draw function

--- src/Deck.js
+++ src/Deck.js
@@ -4,6 +4,15 @@ import React, { useEffect, useState } from "react";
 
 const BASE_URL = "https://deckofcardsapi.com/api/deck";
 
+
+/** Deck component
+ * 
+ * State:
+ *  - deckID
+ *  - cards
+ * 
+ * App -> Deck
+ */
 function Deck() {
   const [deckId, setDeckId] = useState({
     deckId: null,
@@ -11,27 +20,37 @@ function Deck() {
   });
   const [cards, setCards] = useState([]);
 
+
   useEffect(() => {
     async function getDeckID() {
       const result = await axios.get(`${BASE_URL}/new/shuffle`);
       setDeckId({
-        deckId: result.data,
+        deckId: result.data.deck_id,
         isLoading: false,
       });
     }
     getDeckID();
   }, []);
 
+  /** Draw a card, update cards state with new card */
   async function drawCard() {
-    const card = await axios.get(`${BASE_URL}/deck/${deckId}/draw`);
-    setCards([...cards,card])
+    console.log("deckID", deckId);
+    const result = await axios.get(`${BASE_URL}/${deckId.deckId}/draw`);
+    console.log("card result", result);
+    console.log("cards", cards);
+
+    setCards([...cards, result.data.cards[0]]);
   }
 
   if (deckId.isLoading) return <p>...loading...</p>;
   return (
     <>
       <button onClick={drawCard}>Draw Card</button>
-      
+
+      <div>
+        {cards.map(c => <img src={c.image} alt={c.code} key={c.code} />)}
+      </div>
+
     </>
   );
 }
